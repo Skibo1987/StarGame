f6e97f1498b999e442275de4d488bc099944e362
geekbrains <info@geekbrains.ru>
2021-11-18 10:14:48 -0800

    HW1+speed img

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -1,25 +1,39 @@
 package ru.geekbrains;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	TextureRegion region;
+	int x,y;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		region = new TextureRegion(img, 30,40,100,150);
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		x++;
+		y++;
+		ScreenUtils.clear(Color.BROWN);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
+		batch.draw(img, 120,120,50,50);
+		batch.draw(region, 250,400,100,150);
+//		Gdx.graphics.getHeight();
+//		Gdx.graphics.getWidth();
 		batch.end();
 	}
 	
--- desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package ru.geekbrains.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import ru.geekbrains.StarGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new StarGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 500;
+        config.height = 700;
+        config.resizable = false;
+        new LwjglApplication(new StarGame(), config);
+    }
 }
