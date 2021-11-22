48e38635a9f1ad7e99581c3258ee7471141644d6
geekbrains <info@geekbrains.ru>
2021-11-22 04:18:47 -0800

    HW2

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -1,47 +1,15 @@
 package ru.geekbrains;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-public class StarGame extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
-    TextureRegion region;
-    int x, y;
+import com.badlogic.gdx.Game;
 
+
+import ru.geekbrains.screen.impl.MenuScreen;
+
+public class StarGame extends Game {
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
-        region = new TextureRegion(img, 30, 40, 100, 150);
-
-    }
-
-    @Override
-    public void render() {
-        x+=2;
-        y+=2;
-        ScreenUtils.clear(Color.BROWN);
-        batch.begin();
-        batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.draw(img, x, y);
-        batch.draw(img, 120, 120, 50, 50);
-        batch.draw(region, 250, 400, 100, 150);
-
-//		Gdx.graphics.getHeight();
-//		Gdx.graphics.getWidth();
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        img.dispose();
+        setScreen(new MenuScreen());
     }
 }
