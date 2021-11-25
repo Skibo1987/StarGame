61fbc5f9ae0e2111e31b9c9270b874d13203f9d3
geekbrains <info@geekbrains.ru>
2021-11-25 01:08:31 -0800

    HW2_Skibo

--- core/src/ru/geekbrains/screen/impl/MenuScreen.java
+++ core/src/ru/geekbrains/screen/impl/MenuScreen.java
@@ -7,26 +7,36 @@ import com.badlogic.gdx.math.Vector2;
 import ru.geekbrains.screen.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
+    private final float V_LEN = 0.5f;
+
+
     private Texture img;
     private Vector2 touch;
     private Vector2 v;
+    private Vector2 pos;
+    private  Vector2 tmp;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
+        pos = new Vector2();
         touch = new Vector2();
-        v = new Vector2(1, 1);
+        v = new Vector2();
+        tmp = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
         batch.begin();
-
-        batch.draw(img, touch.x,touch.y);
+        batch.draw(img, pos.x, pos.y);
         batch.end();
-        touch.add(v);
+        tmp.set(touch);
+        if (tmp.sub(pos).len() > V_LEN) {
+            pos.add(v);
+
+        }
     }
 
     @Override
@@ -38,6 +48,8 @@ public class MenuScreen extends BaseScreen {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(touch.cpy().sub(pos)).setLength(V_LEN);
         return super.touchDown(screenX, screenY, pointer, button);
     }
+
 }
