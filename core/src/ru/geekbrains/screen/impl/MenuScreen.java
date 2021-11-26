754b2d4b2b51218770c88b50e709faa55afb2545
geekbrains <info@geekbrains.ru>
2021-11-26 00:34:51 -0800

    Lesson_3

--- core/src/ru/geekbrains/screen/impl/MenuScreen.java
+++ core/src/ru/geekbrains/screen/impl/MenuScreen.java
@@ -4,40 +4,66 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
+import ru.geekbrains.math.Rect;
 import ru.geekbrains.screen.BaseScreen;
+import ru.geekbrains.sprite.Logo;
+import ru.geekbrains.sprite.impl.Background;
 
 public class MenuScreen extends BaseScreen {
     private Texture img;
-    private Vector2 touch;
-    private Vector2 v;
+    private Texture bg;
+
+
+    private Background background;
+    private Logo logo;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
-        touch = new Vector2();
-        v = new Vector2(1, 1);
+        bg = new Texture("bg.png");
+
+
+        background = new Background(bg);
+        logo = new Logo(img);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        logo.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        batch.begin();
+        update(delta);
+        draw();
 
-        batch.draw(img, touch.x,touch.y);
-        batch.end();
-        touch.add(v);
     }
 
     @Override
     public void dispose() {
         super.dispose();
         img.dispose();
+        bg.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        return super.touchDown(screenX, screenY, pointer, button);
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        logo.touchDown(touch, pointer, button);
+        return false;
+    }
+
+    private void update(float delta) {
+        logo.update(delta);
+    }
+
+    private void draw() {
+        batch.begin();
+        background.draw(batch);
+        logo.draw(batch);
+        batch.end();
     }
 }
